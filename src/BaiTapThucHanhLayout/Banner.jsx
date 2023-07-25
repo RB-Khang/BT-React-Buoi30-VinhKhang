19c648e0bfb1919bb21d2b91781aebb82fa79e1c
Khang <vinhkhang.tl@gmail.com>
2023-07-26 00:06:50 +0700

    sua loi linh tinh

--- src/BaiTapThucHanhLayout/Banner.jsx
+++ src/BaiTapThucHanhLayout/Banner.jsx
@@ -6,9 +6,9 @@ const Banner = () => {
             <div className="container px-lg-5">
                 <div className="p-4 p-lg-5 bg-light rounded-3 ">
                     <div className="m-4 m-lg-5">
-                        <h1 className="display-5 fw-bold ">A warm welcome!</h1>
-                        <p className="fs-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sequi, nam, dolor omnis vitae recusandae nesciunt blanditiis atque tempora eius ipsam, dolorem praesentium cumque quisquam.</p>
-                        <a className="btn btn-primary btn-lg text-left" href="#!">Call to action</a>
+                        <h1 className="display-2 fw-normal ">A Warm Welcome!</h1>
+                        <p className="fs-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sequi, nam, dolor omnis vitae recusandae nesciunt blanditiis atque tempora eius ipsam, dolorem praesentium cumque quisquam.</p>
+                        <a className="btn btn-primary btn-lg text-left" href="#!">Call to action!</a>
                     </div>
                 </div>
             </div>
